5f3bf1b4ab80235de5675b91833861216a0645a4
Tom Dudfield <tom@dudfield.co.uk>
2013-04-17 12:53:03 +0100

    add solution

---
+++ UnoTV.Web/Controllers/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UnoTV.Web.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UnoTV.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UnoTV.Web.MvcApplication" Language="C#" %>
