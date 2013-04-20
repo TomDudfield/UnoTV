38335288c9bed8f8af19ed6261dd3cfda3c8d20e
Tom Dudfield <tom@dudfield.co.uk>
2013-04-20 12:01:04 +0100

    add objects

--- UnoTV.Web/Hubs/GameHub.cs
+++ UnoTV.Web/Hubs/GameHub.cs
@@ -7,46 +7,51 @@ using Microsoft.AspNet.SignalR;
 
 namespace UnoTV.Web.Hubs
 {
-    public class Message
-    {
-        public string PersonName { get; set; }
-
-        public string Data { get; set; }
-    }
-    
     public class GameHub : Hub
     {
-        private static readonly List<Message> Messages = new List<Message>();
-        private static readonly Dictionary<string, string> Players = new Dictionary<string, string>();
+        public void Join(string playerName)
+        {
+            Clients.All.playerJoined(playerName);
+        }
+
+        public void StartGame()
+        {
+            Clients.All.gameStarted();
+
+            Clients.All.deal();
+        }
+
+        //private static readonly List<Message> Messages = new List<Message>();
+        //private static readonly Dictionary<string, string> Players = new Dictionary<string, string>();
         
-        public void Send(Message message)
-        {
-            message.PersonName += " (" + Context.ConnectionId + ")";
-            Messages.Add(message);
-            Clients.All.addNewMessageToPage(message);
-        }
+        //public void Send(Message message)
+        //{
+        //    message.PersonName += " (" + Context.ConnectionId + ")";
+        //    Messages.Add(message);
+        //    Clients.All.addNewMessageToPage(message);
+        //}
 
-        public void Join(string personName)
-        {
-            Players.Add(Context.ConnectionId, personName);
-            Clients.All.playerJoined(personName);
-        }
+        //public void Join(string personName)
+        //{
+        //    Players.Add(Context.ConnectionId, personName);
+        //    Clients.All.playerJoined(personName);
+        //}
 
-        public void GetAll()
-        {
-            Clients.Caller.allMessages(Messages);
-        }
+        //public void GetAll()
+        //{
+        //    Clients.Caller.allMessages(Messages);
+        //}
 
-        public override Task OnDisconnected()
-        {
+        //public override Task OnDisconnected()
+        //{
 
-            if (Players.ContainsKey(Context.ConnectionId))
-            {
-                Clients.All.playerLeft(Players[Context.ConnectionId]);
-                Players.Remove(Context.ConnectionId);
-            }
+        //    if (Players.ContainsKey(Context.ConnectionId))
+        //    {
+        //        Clients.All.playerLeft(Players[Context.ConnectionId]);
+        //        Players.Remove(Context.ConnectionId);
+        //    }
 
-            return base.OnDisconnected();
-        }
+        //    return base.OnDisconnected();
+        //}
     }
 }
