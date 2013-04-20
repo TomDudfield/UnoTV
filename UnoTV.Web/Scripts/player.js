bfde01c6af8493e8fabb2c0d5a31b49e84f28244
Luke Guppy <luke@guppycreative.com>
2013-04-20 12:26:15 +0100

    script files added

---
+++ UnoTV.Web/Scripts/player.js
@@ -0,0 +1,53 @@
+﻿var hand = {
+    "hand": {
+        "cards": [{
+            "value": 1,
+            "colour": "red",
+            "playable": true,
+            "pickedUp": false
+        },
+        {
+            "value": 1,
+            "colour": "red",
+            "playable": true,
+            "pickedUp": false
+        },
+        {
+            "value": 1,
+            "colour": "red",
+            "playable": true,
+            "pickedUp": false
+        },
+        {
+            "value": 1,
+            "colour": "red",
+            "playable": true,
+            "pickedUp": false
+        },
+        {
+            "value": 1,
+            "colour": "red",
+            "playable": true,
+            "pickedUp": false
+        },
+        {
+            "value": 1,
+            "colour": "red",
+            "playable": true,
+            "pickedUp": false
+        },
+        {
+            "value": 1,
+            "colour": "red",
+            "playable": true,
+            "pickedUp": true
+        }],
+        "playerActive": true,
+        "playerName": "{}",
+        "points": NaN,
+        "currentCard": {
+            "value": 1,
+            "colour": "red"
+        }
+    }
+};
---
+++ UnoTV.Web/Scripts/table.js
@@ -0,0 +1,26 @@
+﻿var tableData = {
+    "table": {
+        "players": [{
+            "name": "{}",
+            "active": true
+        },
+        {
+            "name": "{}",
+            "active": true
+        },
+        {
+            "name": "{}",
+            "active": true
+        },
+        {
+            "name": "{}",
+            "active": true
+        }],
+        "gameActive": true,
+        "card": {
+            "value": 1,
+            "colour": "{}"
+        }
+    }
+};
+
