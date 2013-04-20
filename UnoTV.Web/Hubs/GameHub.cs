80ba2e48a173ebdbe67eedea00ee27a9ce0e4487
Tom Dudfield <tom@dudfield.co.uk>
2013-04-20 15:46:30 +0100

    remove check

--- UnoTV.Web/Hubs/GameHub.cs
+++ UnoTV.Web/Hubs/GameHub.cs
@@ -21,9 +21,6 @@ namespace UnoTV.Web.Hubs
 
         public void StartGame()
         {
-            if (Game.Players.Count < 2)
-                return;
-
             Game.Start();
             Clients.All.gameStarted(); // all clients listen
 
