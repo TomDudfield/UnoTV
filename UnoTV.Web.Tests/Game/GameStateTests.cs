a6cf0dd32795b87c93ec27bf1f6b376658f8238c
Peter Keating <peter@moov2.com>
2013-04-20 12:10:56 +0100

    added GameState which has the ability to add players.

---
+++ UnoTV.Web.Tests/Game/GameStateTests.cs
@@ -0,0 +1,25 @@
+﻿
+using NUnit.Framework;
+using UnoTV.Web.Domain;
+using UnoTV.Web.Game;
+namespace UnoTV.Web.Tests.Game
+{
+    public class GameStateTests
+    {
+        private GameState _gameState;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _gameState = new GameState();
+        }
+
+        [Test]
+        public void AddPlayer_AddsNewPlayerToTheGame()
+        {
+            var newPlayer = new Player();
+            _gameState.AddPlayer(newPlayer);
+            Assert.That(_gameState.Players.Contains(newPlayer), Is.True);
+        }
+    }
+}
