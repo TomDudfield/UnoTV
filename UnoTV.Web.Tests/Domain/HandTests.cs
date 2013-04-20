5cea4954338943a4931478603778f1b063c539b1
Peter Keating <peter@moov2.com>
2013-04-20 14:21:58 +0100

    added RemoveCard method to Hand.

---
+++ UnoTV.Web.Tests/Domain/HandTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnoTV.Web.Domain;
+
+namespace UnoTV.Web.Tests.Domain
+{
+    public class HandTests
+    {
+        [Test]
+        public void RemoveCard_RemovesCardFromHand()
+        {
+            var hand = new Hand();
+            hand.PlayableCards = new List<PlayableCard> { new PlayableCard(new Card { Colour = CardColour.Blue, Value = 1 }) };
+
+            hand.RemoveCard(new Card { Colour = CardColour.Blue, Value = 1 });
+            Assert.That(hand.PlayableCards.Count, Is.EqualTo(0));
+        }
+    }
+}
