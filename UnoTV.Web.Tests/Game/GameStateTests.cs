37f6d3baebd74727c39e14fd692d114868ae7937
Tom Dudfield <tom@dudfield.co.uk>
2013-04-20 14:00:05 +0100

    play card

--- UnoTV.Web.Tests/Game/GameStateTests.cs
+++ UnoTV.Web.Tests/Game/GameStateTests.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Linq;
 using NUnit.Framework;
 using UnoTV.Web.Domain;
@@ -21,10 +22,45 @@ namespace UnoTV.Web.Tests.Game
             var id = "{E6AB01BE-E623-492C-8390-01786604DD14}";
             var name = "Bob";
             var newPlayer = new Player(id, name);
+
             _gameState.AddPlayer(newPlayer);
+
             Assert.That(_gameState.Players.Contains(newPlayer), Is.True);
             Assert.AreEqual(_gameState.Players.First().Id, id);
             Assert.AreEqual(_gameState.Players.First().Name, name);
+        }
+
+        [Test]
+        public void Start_LessTwoPlayersThrowsException()
+        {
+            Assert.Throws<Exception>(_gameState.Start);
+        }
+
+        [Test]
+        public void Start_SetsInitialValues()
+        {
+            _gameState.AddPlayer(new Player("{E6AB01BE-E623-492C-8390-01786604DD14}", "Bob"));
+            _gameState.AddPlayer(new Player("{18DFB92D-7EF8-45F2-87AD-72FBC9ABE683}", "Tim"));
+
+            _gameState.Start();
+
+            Assert.AreEqual(_gameState.Players.First(), _gameState.CurrentPlayer);
+            Assert.AreEqual(_gameState.PlayedCards.First(), _gameState.CurrentCard);
+        }
+
+        [Test]
+        public void PlayCard_SetsCurrentCardAndPlayedCard()
+        {
+            _gameState.AddPlayer(new Player("{E6AB01BE-E623-492C-8390-01786604DD14}", "Bob"));
+            _gameState.AddPlayer(new Player("{18DFB92D-7EF8-45F2-87AD-72FBC9ABE683}", "Tim"));
+            _gameState.Start();
+            var card = _gameState.Cards.First();
+
+            _gameState.PlayCard(card);
+
+            Assert.IsFalse(_gameState.Cards.Contains(card));
+            Assert.AreEqual(card, _gameState.CurrentCard);
+            Assert.IsTrue(_gameState.PlayedCards.Contains(card));
         }
     }
 }
